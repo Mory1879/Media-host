e744b62db3e988f5084e4d64718c404bff35ffd2
Fedor Kirpichev <kirpichel@gmail.com>
2016-10-22 19:51:10 +0300

    fixed deletion media from server or request

--- server/controllers/webmController.js
+++ server/controllers/webmController.js
@@ -1,8 +1,19 @@
 var Resource = require('resourcejs');
+var fs = require('fs');
+
 module.exports = function(app, route) {
 
   // Setup the controller for REST;
-  Resource(app, '', route, app.models.webm).rest();
+  Resource(app, '', route, app.models.webm).rest({
+    afterDelete: function (req, res, next) {
+      var regex = /(https?:\/\/\S{1,15}\/files\/)(\S*\.\S*)/g;
+      var match = regex.exec(res.resource.item.urlToVideo);
+      console.log(match[2]);
+      var path = '/Users/mory/Documents/Учеба/курсач/Video hosting/files/' + match[2];
+      fs.unlink(path, next);
+      next();
+    }
+  });
 
   // Return middleware.
   return function(req, res, next) {
